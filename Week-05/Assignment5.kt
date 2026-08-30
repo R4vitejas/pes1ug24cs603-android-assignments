fc3ede921622687b86e7022edbcf0680b1e70528
Ravitejas <ravitejasshreedhar@gmail.com>
2026-08-30 14:22:59 +0530

    Update Assignment5.kt

--- Week-05/Assignment5.kt
+++ Week-05/Assignment5.kt
@@ -1,16 +1,45 @@
 package com.pes.lib
 
-// exception handling
-fun getNumber(data: String): Int {
-    try {
-        return data.toInt()
-    } catch (e: NumberFormatException) {
-        println("Invalid number")
-        return 0
+open class BankAccounts(
+    val accName: String,
+    val accNumber: Int,
+    var balance: Int = 0
+) {
+
+    open fun displayInfo() {
+        println("Account Name: $accName")
+        println("Account Number: $accNumber")
+        println("Balance: $balance")
+    }
+}
+
+class SavingAccounts(
+    accName: String,
+    accNumber: Int,
+    var interestRate: Float
+) : BankAccounts(accName, accNumber) {
+
+    fun credit(amount: Int) {
+        balance += amount
+    }
+
+    fun debit(amount: Int) {
+        balance -= amount
+    }
+
+    override fun displayInfo() {
+        println("Account Name: $accName")
+        println("Account Number: $accNumber")
+        println("Balance: $balance")
+        println("Interest Rate: $interestRate")
     }
 }
 
 fun main() {
-    getNumber("12")
-    getNumber("a1as")
+    val account = SavingAccounts("John", 1234, 5.0f)
+
+    account.credit(1000)
+    account.debit(200)
+
+    account.displayInfo()
 }
